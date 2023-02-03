f5493d0a358ab6800081e3eb0cb081ace5abecd8
Ankush Agarwal <ankushagarwal11@gmail.com>
2023-02-02 17:19:06 -0800

    Update to use KMS

--- src/createNFT.ts
+++ src/createNFT.ts
@@ -4,7 +4,7 @@ import { wallet, getPolygonFee } from "./utils";
 
 async function createNFT() {
   // Set coCreateProjectAddr here
-  const coCreateProjectAddr = "0x1e4d7367592185bAE4542E4a94ccc5c779EfBe7e";
+  const coCreateProjectAddr = "0xfB8ca074f815E0e3a8d986f602b83E7095c7A6ce";
   const coCreateProject = CoCreateProject__factory.connect(
     // @ts-ignore
     coCreateProjectAddr,
@@ -14,14 +14,16 @@ async function createNFT() {
   console.log(`coCreateProject ${projectName} at ${coCreateProject.address}`);
 
   console.log("Deploying a NFT Contract");
+  // 0x3FEA205BB94500e0d6d461B3393845F3bA337716 corresponds to kms ccdfae38-a541-47e4-8637-f56717840d27 bulleit-dev
+
   const calldata = CoCreateERC721A__factory.createInterface().encodeFunctionData("initialize", [
     "Test NFT",
     "TST",
     "https://bafybeigeh7c662nadnbm7igsaulo74iwkr6mua344txj54x6rhimttj7fy.ipfs.nftstorage.link/",
-    "0xdf6c46bC8A14Be0a1E1688b7b52732C2781023D1",
+    "0x3FEA205BB94500e0d6d461B3393845F3bA337716",
     0,
     5000,
-    "0xdf6c46bC8A14Be0a1E1688b7b52732C2781023D1",
+    "0x3FEA205BB94500e0d6d461B3393845F3bA337716",
   ]);
   const tx = await coCreateProject.deployProxy("CoCreateERC721A", calldata, await getPolygonFee());
   const receipt = await tx.wait();
@@ -29,6 +31,8 @@ async function createNFT() {
     return x.event == "ProxyDeployed";
   })[0].args?.proxy;
   const erc721A = CoCreateERC721A__factory.connect(erc721AAddr, wallet);
+  // const tx2 = await erc721A.transferOwnership("0x3FEA205BB94500e0d6d461B3393845F3bA337716")
+  // const receipt2 = await tx2.wait();
 
   console.log(
     // @ts-ignore
